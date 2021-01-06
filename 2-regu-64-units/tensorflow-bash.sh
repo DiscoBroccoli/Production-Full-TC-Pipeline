e730b688fb7afad34fb52276c3d3f020c7b793db
Jie Bao <jiebao@gra-login3.graham.sharcnet>
2021-01-05 19:17:30 -0500

    fixed graph and/or naming

---
+++ 2-regu-64-units/tensorflow-bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=4
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=0
+#SBATCH --account=def-vermeire
+#SBATCH --mail-user=jiebao995@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --time=12:00:00
+#SBATCH --job-name=Full-64-2regu-TC
+#SBATCH --output=%j.out
+
+python NN.py
